9eaac7291f7d313fb55e38279fe56f58b050a6fe
Marek Pelowski <marek.pelowski@protonmail.com>
2023-07-17 13:20:04 +0200

    created a few buttons and made the window resizable

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ root = Tk()
 root.geometry("415x590")
 root.title("calculator")
 root.configure(bg="#202020")
-root.resizable(False, False)
+#root.resizable(False, False)
 
 equation_var = StringVar()
 equation_var.set(0)
@@ -29,7 +29,21 @@ buttonframe.columnconfigure(0, weight=1)
 buttonframe.columnconfigure(1, weight=1)
 buttonframe.columnconfigure(2, weight=1)
 buttonframe.columnconfigure(3, weight=1)
-buttonframe.columnconfigure(4, weight=1)
+
+btn_7 = Button(buttonframe, text="7", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_7.grid(row=1, column=0, sticky=W + E)
+
+btn_6 = Button(buttonframe, text="6", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_6.grid(row=1, column=1, sticky=W + E)
+
+btn_5 = Button(buttonframe, text="5", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_5.grid(row=1, column=2, sticky=W + E)
+
+multiplication_sing = Button(buttonframe, text="x", bg="#43484f", font=("Arial", 50), borderwidth=0)
+multiplication_sing.grid(row=1, column=3, sticky=W + E)
+
+
+buttonframe.pack(fill="both")
 
 
 root.mainloop()
