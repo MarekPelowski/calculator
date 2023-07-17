8b2208e6f7fc1581c8c5a109acdf287b531281de
Marek Pelowski <marek.pelowski@protonmail.com>
2023-07-17 12:46:46 +0200

    added a color pallet.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
 
+# colour pallet
+# background: #202020
+# buttons: #3B3B3B
+# mathematical signs: #323232
+# equation sing: #F38064
+
 root = Tk()
-root.geometry("500x500")
+root.geometry("415x530")
 root.title("calculator")
+root.configure(bg="#202020")
 
 
 root.mainloop()
