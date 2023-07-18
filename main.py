4479382a7d893a5b3803348656876ea0609115a6
Marek Pelowski <marek.pelowski@protonmail.com>
2023-07-18 15:04:50 +0200

    added button 0

--- main.py
+++ main.py
@@ -36,34 +36,38 @@ buttonframe.rowconfigure(2, weight=1)
 buttonframe.rowconfigure(3, weight=1)
 
 
-btn_7 = Button(buttonframe, text="7", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_7 = Button(buttonframe, text="7", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_7.grid(row=0, column=0, sticky="nsew")
 
-btn_8 = Button(buttonframe, text="8", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_8 = Button(buttonframe, text="8", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_8.grid(row=0, column=1, sticky="nsew")
 
-btn_9 = Button(buttonframe, text="9", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_9 = Button(buttonframe, text="9", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_9.grid(row=0, column=2, sticky="nsew")
 
 
-btn_4 = Button(buttonframe, text="4", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_4 = Button(buttonframe, text="4", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_4.grid(row=1, column=0, sticky="nsew")
 
-btn_5 = Button(buttonframe, text="5", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_5 = Button(buttonframe, text="5", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_5.grid(row=1, column=1, sticky="nsew")
 
-btn_6 = Button(buttonframe, text="6", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_6 = Button(buttonframe, text="6", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_6.grid(row=1, column=2, sticky="nsew")
 
 
-btn_1 = Button(buttonframe, text="1", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_1 = Button(buttonframe, text="1", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_1.grid(row=2, column=0, sticky="nsew")
 
-btn_2 = Button(buttonframe, text="2", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_2 = Button(buttonframe, text="2", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_2.grid(row=2, column=1, sticky="nsew")
 
-btn_3 = Button(buttonframe, text="3", bg="#43484f", font=("Arial", 50), borderwidth=0)
+btn_3 = Button(buttonframe, text="3", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
 btn_3.grid(row=2, column=2, sticky="nsew")
+
+
+btn_0 = Button(buttonframe, text="0", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
+btn_0.grid(row=3, column=1, sticky="nsew")
 
 
 multiplication_sing = Button(buttonframe, text="x", bg="#43484f", font=("Arial", 50), borderwidth=0)
