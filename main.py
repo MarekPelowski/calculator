5dc00166cb59af03beea5124d3dd49c70a5ec0f5
Marek Pelowski <marek.pelowski@protonmail.com>
2023-07-18 15:36:09 +0200

    added a padding

--- main.py
+++ main.py
@@ -39,60 +39,60 @@ buttonframe.rowconfigure(3, weight=1)
 
 
 btn_7 = Button(buttonframe, text="7", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_7.grid(row=0, column=0, sticky="nsew")
+btn_7.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
 
 btn_8 = Button(buttonframe, text="8", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_8.grid(row=0, column=1, sticky="nsew")
+btn_8.grid(row=0, column=1, sticky="nsew", padx=2, pady=2)
 
 btn_9 = Button(buttonframe, text="9", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_9.grid(row=0, column=2, sticky="nsew")
+btn_9.grid(row=0, column=2, sticky="nsew", padx=2, pady=2)
 
 
 btn_4 = Button(buttonframe, text="4", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_4.grid(row=1, column=0, sticky="nsew")
+btn_4.grid(row=1, column=0, sticky="nsew", padx=2, pady=2)
 
 btn_5 = Button(buttonframe, text="5", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_5.grid(row=1, column=1, sticky="nsew")
+btn_5.grid(row=1, column=1, sticky="nsew", padx=2, pady=2)
 
 btn_6 = Button(buttonframe, text="6", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_6.grid(row=1, column=2, sticky="nsew")
+btn_6.grid(row=1, column=2, sticky="nsew", padx=2, pady=2)
 
 
 btn_1 = Button(buttonframe, text="1", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_1.grid(row=2, column=0, sticky="nsew")
+btn_1.grid(row=2, column=0, sticky="nsew", padx=2, pady=2)
 
 btn_2 = Button(buttonframe, text="2", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_2.grid(row=2, column=1, sticky="nsew")
+btn_2.grid(row=2, column=1, sticky="nsew", padx=2, pady=2)
 
 btn_3 = Button(buttonframe, text="3", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_3.grid(row=2, column=2, sticky="nsew")
+btn_3.grid(row=2, column=2, sticky="nsew", padx=2, pady=2)
 
 
 btn_0 = Button(buttonframe, text="0", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_0.grid(row=3, column=1, sticky="nsew")
+btn_0.grid(row=3, column=1, sticky="nsew", padx=2, pady=2)
 
 btn_clear = Button(buttonframe, text="C", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_clear.grid(row=3, column=0, sticky="nsew")
+btn_clear.grid(row=3, column=0, sticky="nsew", padx=2, pady=2)
 
 btn_comma = Button(buttonframe, text=",", bg="#3B3B3B", font=("Arial", 50), borderwidth=0)
-btn_comma.grid(row=3, column=2, sticky="nsew")
+btn_comma.grid(row=3, column=2, sticky="nsew", padx=2, pady=2)
 
 
 multiplication_sign = Button(buttonframe, text="x", bg="#43484f", font=("Arial", 50), borderwidth=0)
-multiplication_sign.grid(row=0, column=3, sticky="nsew")
+multiplication_sign.grid(row=0, column=3, sticky="nsew", padx=2, pady=2)
 
 subtraction_sign = Button(buttonframe, text="-", bg="#43484f", font=("Arial", 50), borderwidth=0)
-subtraction_sign.grid(row=1, column=3, sticky="nsew")
+subtraction_sign.grid(row=1, column=3, sticky="nsew", padx=2, pady=2)
 
 addition_sign = Button(buttonframe, text="+", bg="#43484f", font=("Arial", 50), borderwidth=0)
-addition_sign.grid(row=2, column=3, sticky="nsew")
+addition_sign.grid(row=2, column=3, sticky="nsew", padx=2, pady=2)
 
 division_sign = Button(buttonframe, text="/", bg="#43484f", font=("Arial", 50), borderwidth=0)
-division_sign.grid(row=3, column=3, sticky="nsew")
+division_sign.grid(row=3, column=3, sticky="nsew", padx=2, pady=2)
 
 
 equation_sign = Button(buttonframe, text="=", bg="#43484f", font=("Arial", 50), borderwidth=0)
-equation_sign.grid(row=0, column=4, sticky="nsew", rowspan=4)
+equation_sign.grid(row=0, column=4, sticky="nsew", rowspan=4, padx=2, pady=2)
 
 
 buttonframe.pack(fill="both", expand=True)
